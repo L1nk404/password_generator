b72ba43ea0d2116c77fd95aab803309a9e6eb41b
l1nk404 <gabrielhmorais98@gmal.com>
2024-05-01 21:40:10 -0300

    Programa pronto

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import random
+import os
 
-ASCII_ART = """\033[92m 
-                    ........                                              
-             .^7YPGBB######BBGPY7^.                                       
-           ^JGB####################BGJ^                                    
-        :YB############################BY:                                 
-       ^P##################################P^                               
-     .P######################################P.                             
-    !########BG5J???J5GB#######################!                            
-   ?######G?^.         .^?G#####################?                           
-  !#####G~                 ~G####################!                          
- .B####J         ...         Y###################B.                         
- ?####Y       !PB###BP!       JGGGGGGGGGGGGG######?                         
- G###B.      5#########5                    .J####G                         
- G###G      :#BBBBBB#BB#:                     B###G                         
- G###B.      5BBBBBBBB#5                    .J####G                         
- ?####Y       !5B##BBP!       JGGG!..     :BB#####?                         
- .B####J         ...         Y#####B#5    !######B.                         
-  !#####G~                 ~G#########P??5B######!                          
-   ?######G?^.         .^?G#####################?                           
-    !########BG5J???Y5GB#######################!                            
-     .P######################################P.                             
-       ^P##################################P^  
-         :YB############################BY:                                 
-            ^JGB####################BGJ^                                    
-              .^7YPGBB######BBGPY7^.                                       
-                     ........                                              
-                                                                                                     
+ASCII_ART = """\033[92m
+                    ........
+             .^7YPGBB######BBGPY7^.
+           ^JGB####################BGJ^
+        :YB############################BY:
+       ^P##################################P^
+     .P######################################P.
+    !########BG5J???J5GB#######################!
+   ?######G?^.         .^?G#####################?
+  !#####G~                 ~G####################!
+ .B####J         ...         Y###################B.
+ ?####Y       !PB###BP!       JGGGGGGGGGGGGG######?
+ G###B.      5#########5                    .J####G
+ G###G      :#BBBBBB#BB#:                     B###G
+ G###B.      5BBBBBBBB#5                    .J####G
+ ?####Y       !5B##BBP!       JGGG!..     :BB#####?
+ .B####J         ...         Y#####B#5    !######B.
+  !#####G~                 ~G#########P??5B######!
+   ?######G?^.         .^?G#####################?
+    !########BG5J???Y5GB#######################!
+     .P######################################P.
+       ^P##################################P^
+         :YB############################BY:
+            ^JGB####################BGJ^
+              .^7YPGBB######BBGPY7^.
+                     ........
+
 
 \033[0m"""
 
@@ -46,8 +47,17 @@ class Color:
     END = '\033[0m'
 
 
+def cls():
+    """ Clear the terminal."""
+    os.system('cls' if os.name == 'nt' else 'clear')  # clear multiplataform
+
+
 def boldGreenText(text):
     return f"{Color.BOLD}{Color.GREEN}{text}{Color.END}"
+
+
+def boldRedText(text):
+    return f"{Color.BOLD}{Color.RED}{text}{Color.END}"
 
 
 def gen_password(password_lenght):
@@ -70,34 +80,53 @@ def gen_password(password_lenght):
     generated_password = ""
 
     for i in range(password_lenght):
+        # Here we take random elements from ASCII_char_list
         rand_char = random.choice(ASCII_char_list)
         generated_password += rand_char
 
     return generated_password
 
 
+def loop_handle():
+    loop_control = input("Deseja gerar outra senha? [y/n]: ")
+
+    if loop_control == "n" or loop_control == "N":
+        print("\n Até mais !!")
+        exit()
+    elif loop_control == "y" or loop_control == "Y":
+        cls()
+        print(ASCII_ART)
+        Execute()
+    else:
+        print(boldRedText(
+            "Entrada inválida, favor digite apenas [y] ou [n]\n"))
+        loop_handle()
+
+
 def Execute():
     """Simply execute the program and return the generated password
     """
     while True:
+
         password_lenght_input = input(
             "Insira a quantidade de caracteres desejados para a senha: ")
 
-        # Execute the gen_password function, and format it with PrettyPrint class
-        generated_password = boldGreenText(
-            gen_password(int(password_lenght_input)))
+        if password_lenght_input.isdigit():  # Here we are verifying if the user input is int type
 
-        print(
-            f"\nA senha gerada é {generated_password}\n")
+            # Execute the gen_password function, and format it with PrettyPrint class
+            generated_password = boldGreenText(
+                gen_password(int(password_lenght_input)))
 
-        loop_control = input("Deseja gerar outra senha? [y/n]: ").lower()
+            print(f"\nA senha gerada é {generated_password}\n")
 
-        # Allow the user exit de program based on loop_control input.
-        if loop_control == "n":
-            print("\n Até mais !!")
-            exit()
         else:
+            print(
+                boldRedText(
+                    "Input inválido, por favor, insira apenas números inteiros positivos.\n")
+            )
             continue
+
+        loop_handle()
 
 
 """ ################################################################################# """
