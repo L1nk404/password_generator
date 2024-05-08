37dfd9569727b270eba043cbb40e9900ba929fb1
l1nk404 <gabrielhmorais98@gmal.com>
2024-05-08 15:02:45 -0300

    programa comentado

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ ASCII_ART = """\033[92m
 
 
 class Color:
-    """Just add colors for strings.
+    """
+    Adiciona cor as strings
     """
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
@@ -48,87 +49,101 @@ class Color:
 
 
 def cls():
-    """ Clear the terminal."""
+    """ Limpa o terminal"""
     os.system('cls' if os.name == 'nt' else 'clear')  # clear multiplataform
 
 
 def boldGreenText(text):
+    """ Printa o texto na cor verde """
     return f"{Color.BOLD}{Color.GREEN}{text}{Color.END}"
 
 
 def boldRedText(text):
+    """ Printa o texto na cor vermelha """
     return f"{Color.BOLD}{Color.RED}{text}{Color.END}"
 
 
 def gen_password(password_lenght):
-    """Generate a password, based on ASCII code list. The function returns
-        a password string in UTF-8 that was converted from ASCII code.
+    """Gera o Password baseado na tabela ASCII.  A função retorna uma string com
+    caracteres randomizados da lista ASCII-char_list
 
     Args:
-        password_lenght (str): The lenght of password that the user inputs
+        password_lenght (str): Tamanho desejado da senha
 
     Returns:
         str: generated password
+
     """
 
-    # The map function maps every item of (chr, range()).
-    # chr return a corresponding character of a conresponding ASCII code.
-    # range(33,126) is the range on ASCII Table for UTF-8 characters, it includes
-    # uppercase, lowercase, numbers and special charactes.
+    # A função 'map' mapeia todos os item em (chr, range())
+    # chr retorna o caractere correspondente da tabela ASCII dado seu código em decimal
+    # range (33,126) é o intervalo em decimal da tabela ASCUU para caractereses UTF-8
+    # Isso inclui Letras maiúsculas e minúsculas, números e caracteres especiais
     # https://www.ascii-code.com
+
+    # criamos uma lista com elementos UTF-8
     ASCII_char_list = list(map(chr, range(33, 126)))
+
+    # string vazia que será preenchida com a senha
     generated_password = ""
 
-    for i in range(password_lenght):
-        # Here we take random elements from ASCII_char_list
+    for _ in range(password_lenght):
+        # Aqui estamos pegando itens randomizados da lista e acrescentando na string
+        # generated_password
         rand_char = random.choice(ASCII_char_list)
         generated_password += rand_char
 
-    return generated_password
+    return generated_password  # Finalmente retornando a string
 
 
 def loop_handle():
+    """Essa função simplesmente controla o loop do menu, perguntando ao usuário
+       se quer continuar a usar o programa, dependendo da resposta ele reinicia o programa
+       ou fecha o mesmo
+    """
 
+    # Pergunta ao usuário se quer continuar a usar o programa
     loop_control = input("\n\nDeseja gerar outra senha? [y/n]: ")
 
     if loop_control == "n" or loop_control == "N":
-        os.system(f"cowsay -f default Tchauzinho !! ! | lolcat")
-        exit()
+        # Mensagem de despedida
+        os.system(f"cowsay -f default Tchauzinho !!! | lolcat")
+        exit()  # Fecha o programa
     elif loop_control == "y" or loop_control == "Y":
-        cls()
+        cls()  # Limpa o terminal
         print(ASCII_ART)
-        Execute()
-    else:
+        Execute()  # Reinicia o programa
+    else:  # Caso input inválido
         print(boldRedText(
             "Entrada inválida, favor digite apenas [y] ou [n]\n"))
-        loop_handle()
+        loop_handle()  # Rechama a funçção
 
 
 def Execute():
-    """Simply execute the program and return the generated password
+    """ Função principal que executa o programa
     """
     while True:
 
         password_lenght_input = input(
             "Insira a quantidade de caracteres desejados para a senha: ")
 
-        if password_lenght_input.isdigit():  # Here we are verifying if the user input is int type
-
-            # Execute the gen_password function, and format it with PrettyPrint class
+        if password_lenght_input.isdigit():  # Verificamos se o input é do tipo inteiro (int)
+            # Executa a fiunção gen_password e formata com a função boldGreenText
             generated_password = boldGreenText(
                 gen_password(int(password_lenght_input)))
 
             print(f"\nA senha gerada é {generated_password}\n")
-            os.system(f"cowsay -f dragon-and-cow Be safe ! | lolcat")
+            # Mensagem com dragão fofo
+            os.system(f"cowsay -f dragon-and-cow Usar senhas fortes, com letras, números e símbolos, aumenta a segurança digital e protege seus dados pessoais contra ataques cibernéticos. Be safe !! | lolcat")
 
-        else:
+        else:  # Caso o input seja inválido, exibe mensagem de erro
             print(
                 boldRedText(
                     "\n\nInput inválido, por favor, insira apenas números inteiros positivos.\n")
             )
             continue
 
-        loop_handle()
+        loop_handle()  # Chama o controle de loop
 
 
 """ ################################################################################# """
